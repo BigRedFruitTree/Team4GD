56e61e76246bea6fa588b131754e4e31afecbdc2
BigRedFruitTree <blaine.dooley07@gmail.com>
2024-11-13 13:10:42 -0800

    Changed Player Movement and Added Functional Basic Enemy
    Enemy moves side to side with an adjustable speed.

--- Team4/Assets/Scenes/p3t work/BaseEvilguyhealth.cs
+++ Team4/Assets/Scenes/p3t work/BaseEvilguyhealth.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Evilguyhealth : MonoBehaviour
 {
-    [SerializeField] private float maxHealth = 3f;
+    [SerializeField] private float maxHealth = 2f;
 
     [SerializeField] private float currentHealth;
 
     private float speed = 5;
-    
+    private bool FacingRight = false;
+
 
     void Start()
     {
         currentHealth = maxHealth;
-
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
     }
 
     void Update()
@@ -24,7 +25,14 @@ public class Evilguyhealth : MonoBehaviour
             Destroy(gameObject);
         }
 
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        if(FacingRight)
+        {
+          transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        if (!FacingRight)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
 
     }
 
@@ -34,6 +42,31 @@ public class Evilguyhealth : MonoBehaviour
         {
             currentHealth--;
         }
+
+        if (collision.gameObject.tag == "EnemyTurningPoint")
+        {
+            Flip();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Flip();
+        }
+
+    }
+
+    private void Flip()
+    {
+        
+        FacingRight = !FacingRight;
+
+       
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 
 }
