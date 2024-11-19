bbe2e5bfeb7077106d21fea57182afcb335b6fc5
p3tr0ne12 <brandonpetrone9@gmail.com>
2024-11-19 13:41:07 -0800

    hit delay
    - made startup for hit ( https://www.youtube.com/watch?v=JJUnufMLUp0 )

--- Team4/Assets/Scenes/p3t work/script/boss attacking.cs
+++ Team4/Assets/Scenes/p3t work/script/boss attacking.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class bossattacking : MonoBehaviour
 {
+    public bossmovement move;
+    public float hitdelay = 2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        move.moveSpeed = 0f;
+        move.magnitude = 0.3f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hitdelay > 0)
+        {
+            hitdelay -= Time.deltaTime;
+        }
+        else
+        {
+            move.frequency = 0f;
+            hitdelay = 2;
+        }
     }
 }
