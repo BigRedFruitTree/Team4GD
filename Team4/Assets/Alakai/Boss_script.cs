96eb8b7cee98510930d0eeedae55aec6c2c8143f
Micolash710 <freshcrocs710@gmail.com>
2024-11-22 13:40:22 -0800

    stuff

--- Team4/Assets/Alakai/Boss_script.cs
+++ Team4/Assets/Alakai/Boss_script.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
+
+
+
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public GameManager gm;
     //THIS IS FOR THE LEVEL 2 BOSS!!
     public int health = 50;
     public int maxHealth = 50;
     public Slider healthbar;
+    public NavMeshAgent agent;
+    public PlayerMovement1 player;
+    public int DistMin = 10;
+    public int DistMax = 15;
+    public Transform Player;
+
 
     public GameObject attack1HB;
     public float attackTimer = 10;
@@ -25,40 +36,55 @@ public class NewBehaviourScript : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        player = GameObject.Find("Player").GetComponent<PlayerMovement1>();
+        gm = GameObject.Find("game manager").GetComponent<GameManager>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         if (attackTimer > 0)
         {
             attackTimer -= Time.deltaTime;
         }
-        
-        if(attackTimer <= 0 && bossAtacking == false)
-        {
-            attackTimer = 0;    
-        }    
 
-        if(attackTimer <= 0 && bossAtacking == false && attackNumber == 0) 
+        if (attackTimer <= 0 && bossAtacking == false)
         {
-            attackNumber = Random.Range(1,3);
+            attackTimer = 0;
+        }
+
+        if (attackTimer <= 0 && bossAtacking == false && attackNumber == 0)
+        {
+            attackNumber = Random.Range(1, 3);
         }
 
 
-        if(attackTimer <= 0 && bossAtacking == false) 
+        if (attackTimer <= 0 && bossAtacking == false)
         {
-            if(attackNumber == 1)
+            if (attackNumber == 1)
             {
-              attack1HB.SetActive(true);
-              StartCoroutine("AttackLength");
-              bossAtacking = true;
-              attackNumber = 0;
-            } else if(attackNumber == 2)
+                attack1HB.SetActive(true);
+                StartCoroutine("AttackLength");
+                bossAtacking = true;
+                attackNumber = 0;
+            } else if (attackNumber == 2)
             {
-               StartCoroutine("FailedAttackWait");
+                StartCoroutine("FailedAttackWait");
             }
-        }   
+        }
+
+        if (attackTimer >= 0)
+        {
+            float distanceToPlayer = Vector2.Distance(transform.position, Player.position);
+            if (distanceToPlayer <= DistMax)
+            {
+                agent.destination = player.transform.position;
+            }
+
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -72,21 +98,23 @@ public class NewBehaviourScript : MonoBehaviour
 
     IEnumerator AttackLength()
     {
-       
-       yield return new WaitForSeconds(2f);
-       bossAtacking = false;
-       attackTimer = 10;
-       attack1HB.SetActive(false);
+
+        yield return new WaitForSeconds(2f);
+        bossAtacking = false;
+        attackTimer = 10;
+        attack1HB.SetActive(false);
     }
 
 
     IEnumerator FailedAttackWait()
     {
-       
-       yield return new WaitForSeconds(1f);
-       bossAtacking = false;
-       attackTimer = 10;
-       attack1HB.SetActive(false);
-       attackNumber = 0;
+
+        yield return new WaitForSeconds(1f);
+        bossAtacking = false;
+        attackTimer = 10;
+        attack1HB.SetActive(false);
+        attackNumber = 0;
     }
+    
 }
+
