b26976c1af3493e0a6d64b1fe9ea3c8b400e3830
BigRedFruitTree <blaine.dooley07@gmail.com>
2024-11-21 18:50:13 -0800

    Added Basic Attack to LV: 2 Boss
    Every 10 seconds it picks a random number and if is 1, it does the attack. Ideally, it will have more in the future.

--- Team4/Assets/Alakai/Boss_script.cs
+++ Team4/Assets/Alakai/Boss_script.cs
@@ -10,10 +10,15 @@ public class NewBehaviourScript : MonoBehaviour
     public int maxHealth = 50;
     public Slider healthbar;
 
+    public GameObject attack1HB;
+    public float attackTimer = 10;
+    public bool bossAtacking = false;
+    public int attackNumber = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        attack1HB.SetActive(false);
 
         health = maxHealth;
         if (health <= 0)
@@ -25,8 +30,35 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (attackTimer > 0)
+        {
+            attackTimer -= Time.deltaTime;
+        }
         
+        if(attackTimer <= 0 && bossAtacking == false)
+        {
+            attackTimer = 0;    
+        }    
+
+        if(attackTimer <= 0 && bossAtacking == false && attackNumber == 0) 
+        {
+            attackNumber = Random.Range(1,3);
+        }
+
+
+        if(attackTimer <= 0 && bossAtacking == false) 
+        {
+            if(attackNumber == 1)
+            {
+              attack1HB.SetActive(true);
+              StartCoroutine("AttackLength");
+              bossAtacking = true;
+              attackNumber = 0;
+            } else if(attackNumber == 2)
+            {
+               StartCoroutine("FailedAttackWait");
+            }
+        }   
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -36,5 +68,25 @@ public class NewBehaviourScript : MonoBehaviour
             health--;
             healthbar.value = health;
         }
+    }
+
+    IEnumerator AttackLength()
+    {
+       
+       yield return new WaitForSeconds(2f);
+       bossAtacking = false;
+       attackTimer = 10;
+       attack1HB.SetActive(false);
+    }
+
+
+    IEnumerator FailedAttackWait()
+    {
+       
+       yield return new WaitForSeconds(1f);
+       bossAtacking = false;
+       attackTimer = 10;
+       attack1HB.SetActive(false);
+       attackNumber = 0;
     }
 }
